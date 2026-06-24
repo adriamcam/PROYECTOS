6167a04301e0dc9263ba7e4c1c9d301819f013cc
adriamcam <wcambronero@itqscr.com>
2026-06-24 14:46:18 -0600

    Redesign Support Cloud with alert monitoring sidebar

--- obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cfad6130a507b2c45ad3022a340266a6b82abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3795fc0df2d3b0f450541e3a104524e60263fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
