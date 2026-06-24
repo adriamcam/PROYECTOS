8816b471a5322c38a31bdf12dd74792681f84571
adriamcam <wcambronero@itqscr.com>
2026-06-24 15:44:46 -0600

    Clean collapsible sidebar CSS

--- obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94da2a912086b0338161b92a97bb15babb7c903c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1cf0803dec183a08436e293baefabdcce76f4a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
