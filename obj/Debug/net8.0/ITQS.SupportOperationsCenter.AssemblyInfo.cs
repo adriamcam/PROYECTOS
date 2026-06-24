7ee6dbead681ba0730f090d6568bbcdb8c0ac043
adriamcam <wcambronero@itqscr.com>
2026-06-24 12:43:44 -0600

    Add portal module selection

--- obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adbf9b17a580f29190df09aa00f5ae1b947bb46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b44aac39a6f82e6260a34b84de5bce6031a695b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
