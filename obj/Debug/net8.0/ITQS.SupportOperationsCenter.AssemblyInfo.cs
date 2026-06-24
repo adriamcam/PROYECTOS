45de3a7fd711a3d08a97f3308e52098ab3bab370
adriamcam <wcambronero@itqscr.com>
2026-06-24 12:02:06 -0600

    Add login page and portal home

--- obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4719bbe374c26cfd5ac66ba8a67174ec8312cb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91790d1d68e32d05b65cb6b3abd9854ae1e3f8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
