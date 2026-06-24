733f396bdd394d050eb68d448f7fdd3de0662725
adriamcam <wcambronero@itqscr.com>
2026-06-24 14:56:01 -0600

    Make alert monitoring menu collapsible

--- obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITQS.SupportOperationsCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3795fc0df2d3b0f450541e3a104524e60263fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6167a04301e0dc9263ba7e4c1c9d301819f013cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITQS.SupportOperationsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
