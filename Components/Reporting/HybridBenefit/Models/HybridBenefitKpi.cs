3949df71070a6b9c4e0b113f1e88a4f676c7268f
adriamcam <wcambronero@itqscr.com>
2026-07-04 18:05:54 -0600

    Menu reporteria

--- Components/Reporting/HybridBenefit/Models/HybridBenefitKpi.cs
+++ Components/Reporting/HybridBenefit/Models/HybridBenefitKpi.cs
@@ -10,16 +10,17 @@ public class HybridBenefitKpi
 
     public int WindowsCount { get; set; }
 
+    public int WindowsWithTag { get; set; }
+
+    public int WindowsMissingTag { get; set; }
+
     public int SqlCount { get; set; }
 
-    public int TagCount { get; set; }
+    public int SqlWithTag { get; set; }
 
-    public int MissingAhubTagCount { get; set; }
-
-    public int MissingSqlAhubTagCount { get; set; }
+    public int SqlMissingTag { get; set; }
 
     public int ChangeCount { get; set; }
 
     public DateTime LastScanDate { get; set; }
 }
-
