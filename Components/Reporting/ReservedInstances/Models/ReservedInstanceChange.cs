eb1f734bee5ff5c3cd2ed7b4372f34c4a701b52f
adriamcam <wcambronero@itqscr.com>
2026-07-05 19:53:42 -0600

    mODULO ir

--- Components/Reporting/ReservedInstances/Models/ReservedInstanceChange.cs
+++
@@ -1,8 +0,0 @@
-namespace ITQS.SupportOperationsCenter.Components.Reporting.ReservedInstances.Models;
-
-public sealed class ReservedInstanceChange
-{
-    public string ResourceName { get; set; } = "";
-    public string ChangeType { get; set; } = "";
-    public DateTime? ChangeDate { get; set; }
-}
